339d94080bd4028c2e6fd5cd016aa5594eae2dd0
pournima-r <156657215+pournima-r@users.noreply.github.com>
2025-05-30 17:25:51 +0530

    chapter10

---
+++ chap10/src/pack1/Main.java
@@ -0,0 +1,21 @@
+package pack1;
+
+public class Main {
+ public static void main(String[] args) {
+	 int a=10;
+	 String a1=String.valueOf(a);
+	 String name="Pournima";
+	 String name1="Hrushikesh";
+	 String s1="Hello ";
+	 String s="  How are you  ";
+	 String s2="Good Game";
+	 String replaceString=s2.replace('G', 'H');
+	 System.out.println(name.length());
+	 System.out.println(name1.length());
+	 System.out.println(name.compareTo(name1));
+	 System.out.println(s1.concat(s));
+	 System.out.println(s.trim());
+	 System.out.println(replaceString);
+	System.out.println(a1+10);
+}
+}
---
+++ chap10/src/pack1/Main1.java
@@ -0,0 +1,15 @@
+package pack1;
+class A{
+    int a=30;
+    Integer i=new Integer(a);
+    
+    Integer t=i.intValue();
+	
+	
+}
+public class Main1 {
+ public static void main(String[] args) {
+	A a=new A();
+	System.out.println(a.a);
+}
+}
